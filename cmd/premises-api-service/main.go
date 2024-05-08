9613b4917a483591844bf08dbb7b8dbc91fd7cc5
Viktor Klima <viktorklima@yahoo.com>
2024-05-08 12:13:26 +0200

    initial commit

---
+++ cmd/premises-api-service/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "log"
+    "os"
+    "strings"
+    "github.com/gin-gonic/gin"
+    "github.com/vecnehladny/premises-webapi/api"
+)
+
+func main() {
+    log.Printf("Server started")
+    port := os.Getenv("PREMISES_API_PORT")
+    if port == "" {
+        port = "8080"
+    }
+    environment := os.Getenv("PREMISES_API_ENVIRONMENT")
+    if !strings.EqualFold(environment, "production") { // case insensitive comparison
+        gin.SetMode(gin.DebugMode)
+    }
+    engine := gin.New()
+    engine.Use(gin.Recovery())
+    // request routings
+    engine.GET("/openapi", api.HandleOpenApi)
+    engine.Run(":" + port)
+}
