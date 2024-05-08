b30ab7eb54d60e05c8a5b734f6e0002d3947c0bd
Viktor Klima <viktorklima@yahoo.com>
2024-05-08 12:52:26 +0200

    add mongo db compse file

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -10,13 +10,24 @@ $ProjectRoot = "${PSScriptRoot}/.."
 
 $env:PREMISES_API_ENVIRONMENT="Development"
 $env:PREMISES_API_PORT="8080"
+$env:AMBULANCE_API_MONGODB_USERNAME="root"
+$env:AMBULANCE_API_MONGODB_PASSWORD="root"
+
+function mongo {
+    docker compose --file ${ProjectRoot}/deployments/docker-compose/compose.yaml $args
+}
 
 switch ($command) {
-    "start" {
-        go run ${ProjectRoot}/cmd/premises-api-service
-    }
     "openapi" {
         docker run --rm -ti -v ${ProjectRoot}:/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
+    }
+    "start" {
+            mongo up --detach
+            go run ${ProjectRoot}/cmd/ambulance-api-service
+            mongo down
+        }
+    }
+    mongo up
     }
     default {
         throw "Unknown command: $command"
